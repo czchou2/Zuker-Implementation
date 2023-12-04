20f26d1bfff0784d790233ee0236cea8693cbfbf
Ponyokatsu <brendandeleon13@gmail.com>
2023-12-04 13:43:50 -0600

    Add eH

--- free_energy.py
+++ free_energy.py
@@ -5,4 +5,14 @@ stacking_energies = {"GU": {"GU": -3, "AU": -3, "UA": -3, "CG": -13, "GC": -13},
                      "GC": {"GU": -13, "AU": -21, "UA": -21, "CG": -30, "GC": -48}}
 
 bulge_loop_energies = [0, 28, 39, 45, 50, 52, 53, 55, 56, 57, 58, 59, 61, 62, 63, 64, 65, 67]
-hairpin_loop_energies = {"CG_closing": [0, 999, 999, 84, 59, 41, 43, 45, 46, 48, 49, 50, 52, 53, 54, 55, 57, 59]}
+
+hairpin_loop_energies = {"CG": [999, 999, 84, 59, 41, 43, 45, 46, 48, 49, 50, 52, 53, 54, 55, 57, 59],
+                         "AU" : [999, 999, 80, 75, 69, 64, 66, 68, 69, 70, 71, 73, 74,75,76,77]}
+def eH(i,j,S):
+    size = j - i
+    closing = str(S[i]) + str(S[j])
+    closing = ''.join(sorted(closing))
+    energy = hairpin_loop_energies[closing][size]
+    return energy/10.0
+if __name__ == "__main__":
+    print(eH(0,3,"AAUU"))
