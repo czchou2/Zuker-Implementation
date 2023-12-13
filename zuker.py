67a5a8457c6e73063ac60f0c3b05df0f3f3b9e1f
Claire Chou <czchou2@illinois.edu>
2023-12-13 15:24:51 -0600

    edit dp function

--- zuker.py
+++ zuker.py
@@ -1,12 +1,9 @@
-from free_energy import *
-seq = ""
+from free_energy import eH, eS, eL
+# import sys
 
 
 def parse(filename):
-    '''
-    input: FASTA file
-    output: RNA sequence
-    '''
+    """Parses input FASTA file for RNA sequence."""
     seq = ""
     with open(filename, "r", encoding="UTF-8") as file:
         lines = [line.rstrip() for line in file]
@@ -15,73 +12,85 @@ def parse(filename):
         file.close()
     return seq
 
-def dbn_output(name,dbn,rna,dbn_file):
+
+def dbn_output(name, dbn, rna, dbn_file):
     '''
     input: name string, dot-bracket string, sequence string, file path
     '''
-    f = open(dbn_file,"w")
+    f = open(dbn_file, "w")
     f.write("#Name: "+name + "\n")
-    f.write("#Length: "+str(len(rna)) +"\n")
+    f.write("#Length: "+str(len(rna)) + "\n")
     f.write("#PageNumber: NA\n")
     f.write(rna + "\n")
     f.write(dbn)
     f.close()
 
-def a(i, j):
-    '''
-    input: start and end position
-    output: -1 or 0 depending on whether or not a base pairing
-            between the ith and jth nucleotides is allowed or not
-    '''
-    if seq[i] == 'A' and seq[j] == 'U' \
-            or seq[i] == 'U' and seq[j] == 'A' \
-            or seq[i] == 'C' and seq[j] == 'G' \
-            or seq[i] == 'G' and seq[j] == 'C':
-        return -1
-    return 0
+
+# def a(i, j, S):
+#     """Returns -1 or 0 depending on whether or not a base pairing between the ith and jth nucleotides in S is allowed or not."""
+#     if S[i] == 'A' and S[j] == 'U' \
+#             or S[i] == 'U' and S[j] == 'A' \
+#             or S[i] == 'C' and S[j] == 'G' \
+#             or S[i] == 'G' and S[j] == 'C':
+#         return -1
+#     return 0
 
 
-def dp(seq):
-    n = len(seq)
+def zuker(S):
+    n = len(S)
 
-    # page 609
-    k = 1
+    # TODO:
+    a = 0  # energy contribution for closing of loop
     b = 0
     c = 0
 
-    m = 30
+    # minimal loop size (slide 7)
+    m = 3
+
     # bound on maximal interior loop size?
 
     # minimal energy of general substructure i...j
-    w = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    W = [[0 for _ in range(n+1)] for _ in range(n+1)]
     # minimal energy of closed substructure i...j
-    v = [[float("inf") for _ in range(n+1)] for _ in range(n+1)]
+    V = [[float("inf") for _ in range(n+1)] for _ in range(n+1)]
     # minimal energy of true part of a multi-loop i...j
-    wm = [[float("inf") for _ in range(n+1)] for _ in range(n+1)]
+    WM = [[float("inf") for _ in range(n+1)] for _ in range(n+1)]
 
-    # (n+1) x (n+1) matrix of entries (i, j, matrix)
-    bt = [[None for _ in range(n+1)] for _ in range(n+1)]
+    # initialization:
+    # for j - i <= m, W(i,j)=0, V(i,j)=inf, WM(i,j)=inf
 
+    # recursion:
+    # for i < j - m
     for i in range(n):
         for j in range(n):
-            temp, pos = min([(w[i][k-1] + v[k][j], k)
-                            for k in range(i, j-m-1)])
-            w[i][j] = min(w[i][j-1], temp)
-            bt[i][j] = 
+            # Recursion equation for W
+            eq0 = min([W[i][k-1] + V[k][j]
+                       for k in range(i, j-m)])
+            # corresponds to j unpaired, j paired
+            W[i][j] = min(W[i][j-1], eq0)
 
-            eq1 = min(eH(i, j, seq), v[i+1][j-1] + eS(i, j, seq))
-            eq2 = min([v[i_prime][j_prime] + eL(i, j, i_prime, j_prime, seq)
-                       for i_prime in range(i+1, j)
-                       for j_prime in range(i_prime+1, j)])
-            eq3 = min([wm[i+1][k] + wm[k+1][j-1] + a(i, j)
+            # Recursion equation for V
+            # corresponds to:
+            # hairpin loop, stacking loop
+            # interior loop/bulge
+            # multi-loop
+            eq1 = min(eH(i, j, S), V[i+1][j-1] + eS(i, j, S))
+            eq2 = min([V[ip][jp] + eL(i, j, ip, jp, S)
+                       for ip in range(i+1, j)
+                       for jp in range(ip+1, j)])
+            eq3 = min([WM[i+1][k] + WM[k+1][j-1] + a
                       for k in range(i+1, j)])
-            v[i][j] = min(eq1, eq2, eq3)
+            V[i][j] = min(eq1, eq2, eq3)
 
-            eq4 = min(wm[i][j-1] + c, wm[i+1][j] + c, v[i][j] + b)
-            eq5 = min([wm[i][k] + wm[k+1][j] for k in range(i+1, j)])
-            wm[i][j] = min(eq4, eq5)
+            # Recursion equation for WM
+            # corresponds to:
+            # j unpaired, i unpaired, closed
+            # non-closed
+            eq4 = min(WM[i][j-1] + c, WM[i+1][j] + c, V[i][j] + b)
+            eq5 = min([WM[i][k] + WM[k+1][j] for k in range(i+1, j)])
+            WM[i][j] = min(eq4, eq5)
 
-    return w[1][n], bt
+    return W[1][n]
 
 
 def backtrace(bt):
@@ -91,8 +100,14 @@ def backtrace(bt):
 def main():
     file = "bpRNA_CRW_296.fasta"
     seq = parse(file)
-    score, bt = dp(seq)
-    structure = backtrace(bt)
+    score = zuker(seq)
+    print(score)
+    # structure = backtrace(bt)
+
+    # dbn_file = sys.argv[1]
+    # dbn = "((..))"
+    # rna = "AAGATT"
+    # dbn_output("test",dbn,rna,dbn_file)
 
 
 if __name__ == "__main__":
