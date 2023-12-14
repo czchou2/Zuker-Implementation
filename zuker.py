a7962dc6854255dad908c97cbc811855de8e2f11
Ponyokatsu <brendandeleon13@gmail.com>
2023-12-14 14:15:45 -0600

    Fix W and backtrace

--- zuker.py
+++ zuker.py
@@ -55,21 +55,17 @@ def zuker(S):
     # minimal energy of true part of a multi-loop i...j
     WM = [[float("inf") for _ in range(n)] for _ in range(n)]
 
+    # Wbt = [[None for _ in range(n)] for _ in range(n)]
+    # Vbt = [[None for _ in range(n)] for _ in range(n)]
+    # WMbt = [[None for _ in range(n)] for _ in range(n)]
+    # moves = {"W1","W2"}
     # initialization:
     # for j - i <= m, W(i,j)=0, V(i,j)=inf, WM(i,j)=inf
 
     # recursion:
     # for i < j - m
-    for i in range(n):
-        for j in range(n):
-            if i >= j - m:
-                continue
-
-            # Recursion equation for W
-            eq0 = min([W[i][k-1] + V[k][j]
-                       for k in range(i, j-m)])
-            # corresponds to j unpaired, j paired
-            W[i][j] = min(W[i][j-1], eq0)
+    for j in range(n):
+        for i in reversed(range(j-m)):
 
             # Recursion equation for V
             # corresponds to:
@@ -83,7 +79,6 @@ def zuker(S):
             eq3 = min([WM[i+1][k] + WM[k+1][j-1] + a
                       for k in range(i+1, j)])
             V[i][j] = min(eq1, eq2, eq3)
-
             # Recursion equation for WM
             # corresponds to:
             # j unpaired, i unpaired, closed
@@ -92,79 +87,87 @@ def zuker(S):
             eq5 = min([WM[i][k] + WM[k+1][j] for k in range(i+1, j)])
             WM[i][j] = min(eq4, eq5)
 
+    for j in range(n):
+        for i in reversed(range(j-m)):
+            # Recursion equation for W
+            # eq0 = min([W[i][k-1] + V[k][j]
+            #            for k in range(i, j-m)])
+
+            eq0 = min([W[i][k-1] + V[k][j] if k>0 else V[k][j] for k in range(i,j-m)])
+            # corresponds to j unpaired, j paired
+            W[i][j] = min([W[i][j-1], eq0])
+
     return W, V, WM
 
 
+
 # x = 0 (W), 1 (V), WM (2)
-# def backtrace(i, j, W, V, WM, x):
-#     # temp
-#     m = 3
-#     a = 0
-#     b = 0
-#     c = 0
+def backtrace(i, j, W, V, WM, x,dbn,S):
+    # temp
+    m = 3
+    a = 0
+    b = 0
+    c = 0
 
-#     i = 1
-#     j = len(W)
-#     P = []
-#     while j > 1:
+    if j - m > i: 
+        # backtrack matrix W
+        if x == 0:
+            if W[i][j] == W[i][j-1]:
+                # j unpaired
+                dbn = backtrace(i, j-1, W, V, WM, 0,dbn,S)
+            else:
+                score, k = min([(W[i][k-1] + V[k][j],k) if k>0 else (V[k][j],k) for k in range(i,j-m)])
 
-#         # backtrack matrix W
-#         if x == 0:
-#             if W[i][j] == W[i][j-1]:
-#                 # j unpaired
-#                 j -= 1
-#                 continue
+                if W[i][j] == score:
+                    # j paired
+                    dbn = backtrace(i, k-1, W, V, WM, 0,dbn,S)
+                    dbn = backtrace(k, j, W, V, WM, 1,dbn,S)
+        # backrack matrix V
+        elif x == 1:
+            if V[i][j] == eH(i, j,S):
+                # hairpin loop
+                dbn = dbn[:i] + "(" + dbn[i+1:j] + ")" + dbn[j+1:]
+            elif V[i][j] == V[i+1][j-1] + eS(i, j, S):
+                # stacking loop
+                dbn = dbn[:i] + "(" + dbn[i+1:j] + ")" + dbn[j+1:]
+                dbn = backtrace(i+1, j-1, W, V, WM, 1,dbn,S)
+            else:
+                score, ip, jp = min([(V[ip][jp] + eL(i, j, ip, jp, S), ip, jp)
+                       for ip in range(i+1, j)
+                       for jp in range(ip+1, j)])
+                if V[i][j] == score:
+                    # interior loop or bulge
+                    dbn = dbn[:i] + "(" + dbn[i+1:j] + ")" + dbn[j+1:]
+                    # dbn = dbn[:ip] + "(" + dbn[ip+1:jp] + ")" + dbn[jp+1:] # V will always add closing pair at i,j
+                    dbn = backtrace(ip, jp, W, V, WM, 1,dbn,S)
+                else:
+                    # multiloop
+                    score, k = min([(WM[i+1][k] + WM[k+1][j-1] + a, k)
+                      for k in range(i+1, j)])
+                    dbn = dbn[:i] + "(" + dbn[i+1:j] + ")" + dbn[j+1:]
+                    dbn = backtrace(i+1, k, W, V, WM, 2,dbn,S)
+                    dbn = backtrace(k+1, j-1, W, V, WM, 2,dbn,S)
 
-#             score, k = min([(W[i][k-1] + V[k][j], k)
-#                             for k in range(i, j-m)])
-#             if W[i][j] == score:
-#                 # j paired
-#                 backtrace(i, k-1, W, V, WM, 0)
+        # backtrack matrix WM
+        else:
+            if WM[i][j] == WM[i][j-1] + c:
+                # j unpaired
+                dbn = backtrace(i, j-1, W, V, WM, 2,dbn,S)
+            elif WM[i][j] == WM[i+1][j] + c:
+                # i unpaired
+                dbn = backtrace(i+1, j, W, V, WM, 2,dbn,S)
+            elif WM[i][j] == V[i][j] + b:
+                # closed
+                dbn = backtrace(i,j,W,V,WM,1,dbn,S)
+            else:
+                # unclosed
+                score, k = min([(WM[i][k] + WM[k+1][j], k)
+                      for k in range(i+1, j)])
+                dbn = backtrace(i,k,W,V,WM,2,dbn,S)
+                dbn = backtrace(k+1,j,W,V,WM,2,dbn,S)
 
-#                 backtrace(k, j, W, V, WM, 1)
-#                 # P.append((j, k))
-
-#         # backrack matrix V
-#         elif x == 1:
-#             if V[i][j] == eH(i, j):
-#                 # hairpin loop
-#                 # done?
-#                 return
-#             elif V[i][j] == V[i+1][j-1] + eS(i, j):
-#                 # stacking loop
-#                 i += 1
-#                 j -= 1
-#             else:
-#                 score, ip, jp = min([(V[ip][jp] + eL(i, j, ip, jp, S), ip, jp)
-#                        for ip in range(i+1, j)
-#                        for jp in range(ip+1, j)])
-#                 if V[i][j] == score:
-#                     # interior loop or bulge
-#                     backtrace(ip, jp, W, V, WM, 1)
-#                     # append P?
-
-#                 else:
-#                     # multiloop
-#                     score, k = min([(WM[i+1][k] + WM[k+1][j-1] + a, k)
-#                       for k in range(i+1, j)])
-#                     backtrace(i+1, k, W, V, WM, 2)
-#                     backtrace(k+1, j-1, W, V, WM, 2)
-#                     # append P?
-                    
-
-#         # backtrack matrix WM
-#         else:
-#             if WM[i][j] == WM[i][j-1] + c:
-#                 # j unpaired
-#             elif WM[i][j] == WM[i+1][j] + c:
-#                 # i unpaired
-#             elif WM[i][j] == V[i][j] + b:
-#                 # closed
-#             else:
-#                 # unclosed
-#                 # backtrace
-
-#     return P
+                
+    return dbn
 
 
 def main():
@@ -173,13 +176,13 @@ def main():
 
     print(len(seq))
 
-    S = seq[:150]
+    S = seq[:100]
     starttime = timeit.default_timer()
     W, V, WM = zuker(S)
     print(W[0][len(S)-1], timeit.default_timer() - starttime)
-
-    # structure = backtrace(0, len(S)-1, W, V, WM, 0)
-    # print(structure)
+    dbn = "-" * len(S)
+    dbn = backtrace(0, len(S)-1, W, V, WM, 0,dbn,S)
+    print(dbn)
 
     # dbn_file = sys.argv[1]
     # dbn = "((..))"
@@ -189,3 +192,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # file = "bpRNA_CRW_296.fasta"
+    # seq = parse(file)
+    # test_seq = 'CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG'
+    # W,V, WM = zuker(test_seq)
+    # seq = 'CAAAAAAAAG'
+    # W, V, WM = zuker(seq)
+    # print(W[0][len(test_seq) - 1])
+    # print(W)
